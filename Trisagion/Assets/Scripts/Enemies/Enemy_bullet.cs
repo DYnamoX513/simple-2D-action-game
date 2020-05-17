bc9652a2b2581f2e0eda78b3d9c936180aac04e2
DYnamoX513 <48413978+DYnamoX513@users.noreply.github.com>
2020-05-17 19:58:58 +0800

    Add effects to the bullet of Plant
    Add effects to the bullet of Plant.
    Add animation of blue bird getting hurt.
    Adjust the scale of Plant.

--- Trisagion/Assets/Scripts/Enemies/Enemy_bullet.cs
+++ Trisagion/Assets/Scripts/Enemies/Enemy_bullet.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Enemy_bullet : MonoBehaviour
 {
+    public GameObject fragment1, fragment2;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +20,14 @@ public class Enemy_bullet : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        GameObject f1 = Instantiate(fragment1);
+        GameObject f2 = Instantiate(fragment2);
+        f1.transform.position = gameObject.transform.position + new Vector3(0, 0.1f, 0);
+        f2.transform.position = gameObject.transform.position + new Vector3(0, -0.1f, 0);
+        f1.GetComponent<Rigidbody2D>().velocity = 
+            f2.GetComponent<Rigidbody2D>().velocity = 
+            gameObject.GetComponent<Rigidbody2D>().velocity;
+        
         Destroy(gameObject);
     }
 
